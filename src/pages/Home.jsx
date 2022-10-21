3f29ebbbf8a61a4a6d57da95c1cc8e6e570819c5
JosefKru <josefkaru@gmail.com>
2022-10-21 20:31:06 +0300

    testing redux toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ import Categories from './../Components/Categories/Categories'
 import Sort from './../Components/Sort/Sort'
 import Pagination from './../Components/Pagination/index'
 import '../scss/app.scss'
+import { useSelector, useDispatch } from 'react-redux'
+import { setCategoryId } from '../redux/slices/filterSlice'
 
 function Home() {
   const [items, setItems] = useState([])
@@ -18,12 +20,22 @@ function Home() {
   })
   const { searchValue } = useContext(SearchContext)
 
+  const sss = useSelector((state) => state.filter.categoryId)
+  console.log(sss)
+  // console.log(categoryId)
+  // const dispatch = useDispatch()
+
+  // const onClickCategory = (id) => {
+  //   dispatch(setCategoryId(id))
+  // }
+
   useEffect(() => {
     setIsLoading(true)
 
     const category = categoryId > 0 ? `category=${categoryId}` : ''
     const sortBy = sortType.sortProperty.replace('-', '')
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
+
     fetch(
       `https://6342939b3f83935a78453789.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}`
     ).then((response) => {
